36ed9f2a09d85a3608ed8b855ec78aa163a342b5
Sandeep Hebbar <sandeeph@gmail.com>
2017-11-03 11:35:38 +0530

    Login Module

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "Sandeep Hebbar",
+             email: "sandeep.hebbar@candelalabs.io",
+             password:              "admin123",
+             password_confirmation: "admin123",
+						 activated: true,
+             admin: true)
